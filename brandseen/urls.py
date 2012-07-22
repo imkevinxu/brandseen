3c98d5285130662662e36171af08653df6d894dc
Kevin Xu <kevin@imkevinxu.com>
2012-07-21 22:15:55 -0700

    First commit

---
+++ brandseen/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls.defaults import * 
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('', 
+    ## Admin
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+
+    ## App URL include
+    url(r'^', include('brandseen_app.appurls')),
+    #url(r'^brandseen/$', include('brandseen.appurls')),
+    
+    ## Static Links
+    #url(r'^', TemplateView.as_view(template_name='home.html'), name='home'),
+    
+)
+
+#urlpatterns += patterns('brandseen_site.brandseen.views',
+#    url(r'^brandseen/$', 'brandseen_view', name='brandseen'),
+#)
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
+) + urlpatterns
